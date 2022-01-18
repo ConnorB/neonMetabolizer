0599ba68b2a077ddf7d95a1128f5e9d673953c34
michelleckelly <michellecatherinekelly@gmail.com>
2022-01-18 17:00:12 -0500

    Modify test script

--- TestScript.R
+++ TestScript.R
@@ -1,4 +1,5 @@
 # Testing script
+library (ggplot2)
 
 #### Define parameters for data pull ##########################################
 NEONsites <- c("KING")
@@ -6,15 +7,13 @@ startdate <- "2019-01"
 enddate <- "2019-12"
 
 #### Pull raw data and K from NEON API ########################################
-#data <- request_NEON(NEONsites, startdate, enddate)
+data <- request_NEON(NEONsites, startdate, enddate)
 
 # Save raw data
 #saveRDS(data, file = "data/Raw_KING_2019-01_2019-12.rds")
 # Load
-data <- readRDS(file = "data/Raw_KING_2019-01_2019-12.rds")
 
 #### Visualize raw data #######################################################
-library(ggplot2)
 # Discharge v. K600
 ggplot(data = data$k600_clean, aes(x = meanQ, y = k600)) +
   geom_smooth(method = "lm", color = "black") +
@@ -40,20 +39,30 @@ ggplot(data = data$data, aes(x = solarTime, y = DO_mgL,
   theme(axis.text = element_text(color = "black"),
         panel.background = element_rect(color = "black"),
         legend.position = "bottom")
-# Time period for two station modeling
-ggplot(data = data$data, aes(x = solarTime, y = modelingStrategy,
-                           color = modelingStrategy)) +
-  #facet_grid(rows = vars(horizontalPosition)) +
-  geom_line(alpha = 0.8, color = "grey") +
-  geom_point(shape = 21, fill = "white") +
+
+#### Fit 2 station metabolism model to raw data ###############################
+results <- fit_twostation(data, nbatch = 1e4)
+
+ggplot(data = results$results, aes(x = date)) +
+  geom_ribbon(aes(ymin = GPP.lower, ymax= GPP.upper, fill = "GPP"), alpha = 0.5) +
+  geom_line(aes(y = GPP, color = "GPP"))+
+  geom_point(aes(y = GPP, color = "GPP"), shape = 21, fill = "white") +
   # Theme adjustments
-  scale_x_datetime(date_breaks = "month", date_labels = "%b %Y") +
+  scale_x_date(date_breaks = "month", date_labels = "%b %Y") +
   scale_color_brewer(palette = 6, type = "qual") +
   theme_classic() +
   theme(axis.text = element_text(color = "black"),
         panel.background = element_rect(color = "black"),
         legend.position = "bottom")
 
-#### Fit 2 station metabolism model to raw data ###############################
-
-
+ggplot(data = results$results, aes(x = date)) +
+  geom_ribbon(aes(ymin = K.lower, ymax= K.upper, fill = "K"), alpha = 0.5) +
+  geom_line(aes(y = K, color = "K"))+
+  geom_point(aes(y = K, color = "K"), shape = 21, fill = "white") +
+  # Theme adjustments
+  scale_x_date(date_breaks = "month", date_labels = "%b %Y") +
+  scale_color_brewer(palette = 6, type = "qual") +
+  theme_classic() +
+  theme(axis.text = element_text(color = "black"),
+        panel.background = element_rect(color = "black"),
+        legend.position = "bottom")
